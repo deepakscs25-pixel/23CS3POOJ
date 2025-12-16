576bceef3168a885f74fd24bd8943380d929b025
deepakscs25-pixel <deepaks.cs25@bmsce.ac.in>
2025-12-16 12:01:53 +0530

    Add files via upload

---
+++ LAB CIE/Main.java
@@ -0,0 +1,39 @@
+interface Resizable{
+    void resizeWidth(int width);
+    void resizeHeight(int height);
+    void display();
+}
+
+class Triangle implements Resizable{
+    int width;
+    int height;
+    
+    Triangle(int width,int height){
+        this.width=width;
+        this.height=height;
+    }
+
+    
+    public void resizeHeight(int height) {
+       this.height=height;
+    }
+
+    public void resizeWidth(int width){
+        this.width=width;
+    }
+
+    public void display(){
+        System.out.print("Value of Width:"+width);
+        System.out.print("\nValue of Height:"+height+"\n");
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Resizable t=new Triangle(3,4);
+        t.display();
+        t.resizeHeight(6);
+        t.resizeWidth(8);
+        t.display();
+    }
+}
